fee8cc53a9a9a8eb0e432fb15caf2144a7afd35d
GryfonN <petranikpeter@gmail.com>
2015-04-01 10:42:46 +0200

    gulp serve argument browser

--- gulp/server.js
+++ gulp/server.js
@@ -3,57 +3,59 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var browserSyncSpa = require('browser-sync-spa');
+var argv = require('yargs').argv;
 
 var util = require('util');
 
 var middleware = require('./proxy');
 
-module.exports = function(options) {
+module.exports = function (options) {
 
-  function browserSyncInit(baseDir, browser) {
-    //gryfonn-gulp
-    //browser = browser === undefined ? 'default' : browser;
-    browser = browser === undefined ? 'firefox' : browser;
-    var routes = null;
-    if(baseDir === options.src || (util.isArray(baseDir) && baseDir.indexOf(options.src) !== -1)) {
-      routes = {
-        '/bower_components': 'bower_components'
-      };
+    function browserSyncInit(baseDir, browser) {
+        //gryfonn-gulp
+        //browser = browser === undefined ? 'default' : browser;
+        browser = browser === undefined ? 'firefox' : browser;
+        var routes = null;
+        if (baseDir === options.src || (util.isArray(baseDir) && baseDir.indexOf(options.src) !== -1)) {
+            routes = {
+                '/bower_components': 'bower_components'
+            };
+        }
+
+        var server = {
+            baseDir: baseDir,
+            routes: routes
+        };
+
+        if (middleware.length > 0) {
+            server.middleware = middleware;
+        }
+
+        browserSync.instance = browserSync.init({
+            startPath: '/',
+            server: server,
+            browser: browser
+        });
     }
 
-    var server = {
-      baseDir: baseDir,
-      routes: routes
-    };
+    browserSync.use(browserSyncSpa({
+        selector: '[ng-app]'// Only needed for angular apps
+    }));
 
-    if(middleware.length > 0) {
-      server.middleware = middleware;
-    }
-
-    browserSync.instance = browserSync.init({
-      startPath: '/',
-      server: server,
-      browser: browser
+    gulp.task('serve', ['watch'], function () {
+        var argBrowser = argv.browser || 'firefox';
+        browserSyncInit([options.tmp + '/serve', options.src], argBrowser);
     });
-  }
 
-  browserSync.use(browserSyncSpa({
-    selector: '[ng-app]'// Only needed for angular apps
-  }));
+    gulp.task('serve:dist', ['build'], function () {
+        browserSyncInit(options.dist);
+    });
 
-  gulp.task('serve', ['watch'], function () {
-    browserSyncInit([options.tmp + '/serve', options.src]);
-  });
+    gulp.task('serve:e2e', ['inject'], function () {
+        browserSyncInit([options.tmp + '/serve', options.src], []);
+    });
 
-  gulp.task('serve:dist', ['build'], function () {
-    browserSyncInit(options.dist);
-  });
-
-  gulp.task('serve:e2e', ['inject'], function () {
-    browserSyncInit([options.tmp + '/serve', options.src], []);
-  });
-
-  gulp.task('serve:e2e-dist', ['build'], function () {
-    browserSyncInit(options.dist, []);
-  });
+    gulp.task('serve:e2e-dist', ['build'], function () {
+        browserSyncInit(options.dist, []);
+    });
 };
