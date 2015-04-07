bcb2ab690bf531c90fb9ba486565ce8b8fe988aa
GryfonN <petranikpeter@gmail.com>
2015-04-07 21:38:26 +0200

    skils ide filtering

--- src/app/section-skills/section-skills-controller.js
+++ src/app/section-skills/section-skills-controller.js
@@ -2,5 +2,71 @@
 
 angular.module('portfolio.sectionSkills')
   .controller('SectionSkillsCtrl', function ($scope) {
+    $scope.skills = [
+      'JavaScript',
+      'AngularJS',
+      'BackboneJS',
+      'NodeJS',
+      'Gulp',
+      'Grunt',
+      'Jade',
+      'Less',
+      'npm',
+      'bower',
+      'Boostrap',
+      'MongoDB',
+      'RequireJS',
+      'JQuery',
+
+      'MySQL',
+      'PostgreSQL',
+      'Microsoft SQL Server',
+      'SQLite',
+      'SQL',
+
+      'Android',
+      'Java SE 6',
+      'Java EE',
+      'Hibernate',
+      'GWT',
+      'Vaadin',
+      'OSGi',
+      'C#',
+      'XNA',
+      'Windows Phone',
+
+      //MWAY special
+      'Jmeter',
+      'REST',
+      'Web Applications',
+      'SSH Client',
+      'Ant',
+      'Ivy',
+      'Curl',
+      'Jenkins CI',
+
+      //DERIVATIVES special
+      'Apache SOLR',
+
+      'Scrum',
+      'JIRA',
+
+      'XML',
+      'JSON',
+      'HTML',
+      'CSS',
+
+      'IntelliJ IDEA',
+      'Eclipse',
+      'Visual Studio 2010',
+
+      'Subversion',
+      'Git',
+
+      'Gimp',
+      'Adobe Illustrator',
+
+      'Linux'
+    ];
 
   });
