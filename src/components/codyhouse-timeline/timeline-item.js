d54448ea064b0bf342155b71c8b8821e94d4284a
GryfonN <petranikpeter@gmail.com>
2015-04-09 21:12:22 +0200

    waypoint efekt do codyhouseTimeline componentu

--- src/components/codyhouse-timeline/timeline-item.js
+++ src/components/codyhouse-timeline/timeline-item.js
@@ -6,4 +6,6 @@
  * Vklada sa ako direktiva do pripraveneho elementu < id="cd-timeline" class="cd-container">
  */
 
+/* v IT-PORTFOLIO som mal este waypoint module na vrchu takze preto mam v TMPL waypoint atributy*/
+
 angular.module('gryfonnLair.codyhouseTimeline', []);
