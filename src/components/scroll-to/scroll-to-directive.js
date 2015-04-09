b3013065b39b99c4079583832ad97a672151659b
GryfonN <petranikpeter@gmail.com>
2015-04-09 21:37:06 +0200

    scrollTo podporuje inak atribut a pridany attr delay sekcia experiences ma angularMaterial Btn

--- src/components/scroll-to/scroll-to-directive.js
+++ src/components/scroll-to/scroll-to-directive.js
@@ -4,10 +4,11 @@
  * Requirements:
  * JQUERY.SCROLLTO.JS
  * https://github.com/flesler/jquery.scrollTo
- * <a href="XXX" scroll-to>
+ * <a href="XXX" scroll-to [scroll-to-dealy='XX']>
  */
 
 /* global $ */
+/* global setTimeout */
 
 angular.module('gryfonnlair.scrollTo', [])
   .directive('scrollTo', function () {
@@ -16,11 +17,17 @@ angular.module('gryfonnlair.scrollTo', [])
       link: function (scope, element, attrs) {
         element.click(function (event) {
           event.preventDefault();
-          var fullUrl = attrs.href;
-          var parts = fullUrl.split('#');
-          var trgt = parts[1];
-
-          $('body').scrollTo($('#' + trgt), 800, {offset: -80});
+          //var fullUrl = attrs.href;
+          //var parts = fullUrl.split('#');
+          //var trgt = parts[1];
+          if (attrs.scrollToDelay) {
+            setTimeout(function () {
+              $('body').scrollTo($(attrs.scrollTo), 800, {offset: -80});
+            }, attrs.scrollToDelay);
+          }
+          else {
+            $('body').scrollTo($(attrs.scrollTo), 800, {offset: -80});
+          }
         });
       }
     };
