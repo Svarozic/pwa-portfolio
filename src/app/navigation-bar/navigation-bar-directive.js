3b77121cebf94a0cadefb5fa14d703dd52b02bc4
GryfonN <petranikpeter@gmail.com>
2015-04-26 16:40:04 +0200

    fix zaatvarania menu

--- src/app/navigation-bar/navigation-bar-directive.js
+++ src/app/navigation-bar/navigation-bar-directive.js
@@ -8,4 +8,22 @@ angular.module('portfolio.navigationBar')
       scope: true,
       templateUrl: 'app/navigation-bar/navigation-bar.html'
     };
+  })
+
+  /*
+   Template nezatvarala menu tak som spravil custom handle situacie. Ak je menu roztiahnute tak
+   znova kliknem button.
+   */
+  .directive('onClickCloseNav', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element) {
+        element.click(function () {
+          var expandedNavMenu = $('#navigation');
+          if (expandedNavMenu.hasClass('collapse') && expandedNavMenu.hasClass('in')) {
+            $('.navbar-header button').click();
+          }
+        });
+      }
+    };
   });
