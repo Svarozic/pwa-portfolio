25ed626260eccd57940384531de08cfc0f6f85ec
GryfonN <petranikpeter@gmail.com>
2015-05-08 21:15:51 +0200

    on last repeat direktiva ako universal komponent

---
+++ src/components/onLastRepeatEvent/on-last-repeat-event.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Ak sa jedna o posledny element v ng-repeat direktive, vyhodit to event s nazvom definovany
+ * v samotnej atributovej direktive
+ * http://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
+ *
+ * Pouzitie:
+ * <img ng-repeat="i in project.images" gl-on-last-repeat="timelineRepeatFinished"/>
+ *
+ */
+
+
+angular.module('gryfonnlair.onLastRepeat', [])
+  .directive('glOnLastRepeat', function ($timeout) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attr) {
+        if (scope.$last === true) {
+          $timeout(function () {
+            scope.$emit(attr.glOnLastRepeat);
+          });
+        }
+      }
+    };
+  });
