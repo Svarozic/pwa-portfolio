ff89d2c6b8be5486aa807e465bfc118d905920d5
GryfonN <petranikpeter@gmail.com>
2015-05-08 21:42:51 +0200

    on last repeat dava aj argumenty element aspon

--- src/components/onLastRepeatEvent/on-last-repeat-event.js
+++ src/components/onLastRepeatEvent/on-last-repeat-event.js
@@ -2,7 +2,7 @@
 
 /**
  * Ak sa jedna o posledny element v ng-repeat direktive, vyhodit to event s nazvom definovany
- * v samotnej atributovej direktive
+ * v samotnej atributovej direktive, a v argumentoch posielam element z kt sa to hodilo
  * http://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
  *
  * Pouzitie:
@@ -18,7 +18,7 @@ angular.module('gryfonnlair.onLastRepeat', [])
       link: function (scope, element, attr) {
         if (scope.$last === true) {
           $timeout(function () {
-            scope.$emit(attr.glOnLastRepeat);
+            scope.$emit(attr.glOnLastRepeat, {element: element});
           });
         }
       }
