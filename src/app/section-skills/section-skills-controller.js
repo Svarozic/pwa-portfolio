1905bfd92939dc1d36729cf82c3bb95e92bebdd2
GryfonN <petranikpeter@gmail.com>
2016-10-22 16:15:35 +0200

    -++vek -skill prec animacka, blednutie ak nie je selectnuty

--- src/app/section-skills/section-skills-controller.js
+++ src/app/section-skills/section-skills-controller.js
@@ -3,21 +3,14 @@
 angular.module('portfolio.sectionSkills')
   .controller('SectionSkillsCtrl', function ($scope, SkillFactory) {
     $scope.skills = [];
+    $scope.filterText = '';
 
     angular.forEach(SkillFactory, function (value) {
       this.push(value.title);
     }, $scope.skills);
 
-    /*
-     Specialne skilli ku ktorym nemam obrazok ale tak by tam boli vidiet
-     */
-    $scope.skills = $scope.skills.concat([
-      //another type of skills
-      'Testing',
-
-      'German',
-      'English',
-      'Slovak',
-      'Kanban'
-    ]);
+    $scope.getSelectedClassName = function (skillName) {
+      return skillName.toLowerCase().indexOf($scope.filterText.toLowerCase()) != -1
+        ? 'label-primary' : 'not-selected';
+    }
   });
