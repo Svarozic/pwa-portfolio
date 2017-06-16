074b9e6209159935237b5077fb041b0afe7df36a
Svarozic <petranikpeter@gmail.com>
2017-06-16 19:20:54 +0200

    -remove html from portfolio section -do not use css from ceevee

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './ceevee-static/css/default.css';
-import './ceevee-static/css/layout.css';
-import './ceevee-static/css/media-queries.css';
-import './ceevee-static/css/magnific-popup.css';
+// import './ceevee-static/css/default.css';
+// import './ceevee-static/css/layout.css';
+// import './ceevee-static/css/media-queries.css';
+// import './ceevee-static/css/magnific-popup.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -12,8 +12,10 @@ import initCeeVee from './initCeeVee';
 import HomeAppendix from './components/HomeSection/HomeAppendix';
 
 ReactDOM.render(<HomeAppendix/>, document.getElementById('pp-home-appendix'));
-ReactDOM.render(<PlaygroundSection />, document.getElementById('pp-playground-section'));
+
 ReactDOM.render(<PortfolioSection />, document.getElementById('pp-portfolio-section'));
+
+ReactDOM.render(<PlaygroundSection />, document.getElementById('pp-playground-section'));
 
 registerServiceWorker();
 
