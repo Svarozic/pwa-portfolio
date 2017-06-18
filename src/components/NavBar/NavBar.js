7430eaef0aff0d2c95e3b3b43a84a5357872ef35
GryfonN <petranikpeter@gmail.com>
2017-06-18 16:17:37 +0200

    -NavBae done

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,64 @@
+import './NavBar.css';
 import React, { Component } from 'react';
 
-export default class NavBar extends Component {
+const $ = window.jQuery || window.$;
 
+export default class NavBar extends Component {
     componentDidMount() {
-        // window.jQuery(document).ready(function ($) {
-        //     $(".button-collapse").sideNav();
-        // });
+        /*----------------------------------------------------*/
+        /* Highlight the current section in the navigation bar
+         ------------------------------------------------------*/
+        const sections = $("section");
+        const navigation_links = $("#nav-wrap a");
+        sections.waypoint({
+            handler: function (event, direction) {
+                let active_section = $(this);
+                if (direction === "up") active_section = active_section.prev();
+                const active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+                navigation_links.parent().removeClass("current");
+                active_link.parent().addClass("current");
+            },
+            offset: '35%'
+        });
+
+
+        /*----------------------------------------------------*/
+        /*	Fade In/Out Primary Navigation
+         ------------------------------------------------------*/
+        $(window).on('scroll', function () {
+            const h = $('#pp-home').height();
+            const y = $(window).scrollTop();
+            const nav = $('#nav-wrap');
+
+            if ((y > h * .20) && (y < h) && ($(window).outerWidth() > 768 )) {
+                nav.fadeOut('fast');
+            }
+            else {
+                if (y < h * .20) {
+                    nav.removeClass('opaque').fadeIn('fast');
+                }
+                else {
+                    nav.addClass('opaque').fadeIn('fast');
+                }
+            }
+        });
     }
 
     render() {
         return (
-            <div>nav</div>
+            <nav id="nav-wrap">
+
+                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+                <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#pp-home">Home</a></li>
+                    <li><a className="smoothscroll" href="#pp-about-container">About</a></li>
+                    <li><a className="smoothscroll" href="#pp-portfolio-container">Portfolio</a></li>
+                    <li><a className="smoothscroll" href="#pp-playground-container">Playground</a></li>
+                </ul>
+
+            </nav>
         );
     }
 }
