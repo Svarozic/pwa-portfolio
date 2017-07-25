c8bbe41093c9a7806132ce140f866163f66d0517
Svarozic <petranikpeter@gmail.com>
2017-07-25 11:36:47 +0200

    -remove obsolete ceevee files -remove not used ext-js files -small cosmetics changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// import './ceevee-static/css/default.css';
-// import './ceevee-static/css/layout.css';
-// import './ceevee-static/css/media-queries.css';
-// import './ceevee-static/css/magnific-popup.css';
 import './materialize-modules.css';
 import 'font-awesome/css/font-awesome.css';
 import './font-librebaskerville/librebaskerville.css';
@@ -15,9 +11,9 @@ import NavBar from './components/NavBar/NavBar';
 import HomeAppendix from './components/HomeSection/HomeAppendix';
 import AboutSection from './components/AboutSection/AboutSection';
 import ResumeSection from './components/ResumeSection/ResumeSection';
-import PlaygroundSection from './components/PlaygroundSection/PlaygroundSection';
 import LanguagesSection from './components/LanguagesSection/LanguagesSection';
 import WorksSection from './components/WorksSection/WorksSection';
+import PlaygroundSection from './components/PlaygroundSection/PlaygroundSection';
 import Footer from './components/Footer/Footer';
 
 
@@ -29,7 +25,6 @@ ReactDOM.render(<ResumeSection />, document.getElementById('pp-resume-container'
 ReactDOM.render(<LanguagesSection />, document.getElementById('pp-languages-container'));
 ReactDOM.render(<WorksSection />, document.getElementById('pp-works-container'));
 ReactDOM.render(<PlaygroundSection />, document.getElementById('pp-playground-container'));
-
 
 ReactDOM.render(<Footer />, document.getElementById('pp-footer-container'));
 
--- src/initExtJsPlugins.js
+++ src/initExtJsPlugins.js
@@ -9,7 +9,7 @@ export default function () {
         /* FitText Settings
          ------------------------------------------------------ */
         setTimeout(function () {
-            $('h1.fittext-responsive-headline').fitText(1, {minFontSize: '40px', maxFontSize: '90px'});
+            $('.pp-hello.jquery-fittext').fitText(1, {minFontSize: '50px', maxFontSize: '90px'});
         }, 100);
 
 
@@ -28,85 +28,5 @@ export default function () {
                 window.location.hash = target;
             });
         });
-
-
-        /*----------------------------------------------------*/
-        /*	Modal Popup
-         ------------------------------------------------------*/
-
-        // $('.item-wrap a').magnificPopup({
-        //
-        //     type: 'inline',
-        //     fixedContentPos: false,
-        //     removalDelay: 200,
-        //     showCloseBtn: false,
-        //     mainClass: 'mfp-fade'
-        //
-        // });
-        //
-        // $(document).on('click', '.popup-modal-dismiss', function (e) {
-        //     e.preventDefault();
-        //     $.magnificPopup.close();
-        // });
-
-
-        /*----------------------------------------------------*/
-        /*	Flexslider
-         /*----------------------------------------------------*/
-        // $('.flexslider').flexslider({
-        //     namespace: 'flex-',
-        //     controlsContainer: '.flex-container',
-        //     animation: 'slide',
-        //     controlNav: true,
-        //     directionNav: false,
-        //     smoothHeight: true,
-        //     slideshowSpeed: 7000,
-        //     animationSpeed: 600,
-        //     randomize: false,
-        // });
-
-
-        /*----------------------------------------------------*/
-        /*	contact form
-         ------------------------------------------------------*/
-        //GRYF remove contact form and reference on PHP endpoint
-        // $('form#contactForm button.submit').click(function () {
-        //
-        //     $('#image-loader').fadeIn();
-        //
-        //     const contactName = $('#contactForm #contactName').val();
-        //     const contactEmail = $('#contactForm #contactEmail').val();
-        //     const contactSubject = $('#contactForm #contactSubject').val();
-        //     const contactMessage = $('#contactForm #contactMessage').val();
-        //
-        //     const data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-        //         '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-        //
-        //     $.ajax({
-        //
-        //         type: 'POST',
-        //         url: 'inc/sendEmail.php',
-        //         data: data,
-        //         success: function (msg) {
-        //
-        //             // Message was sent
-        //             if (msg === 'OK') {
-        //                 $('#image-loader').fadeOut();
-        //                 $('#message-warning').hide();
-        //                 $('#contactForm').fadeOut();
-        //                 $('#message-success').fadeIn();
-        //             }
-        //             // There was an error
-        //             else {
-        //                 $('#image-loader').fadeOut();
-        //                 $('#message-warning').html(msg);
-        //                 $('#message-warning').fadeIn();
-        //             }
-        //
-        //         }
-        //
-        //     });
-        //     return false;
-        // });
     });
 }
