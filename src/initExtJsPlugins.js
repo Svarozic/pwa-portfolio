43dd326a71044d7e4e145651a1b7d7576fab521d
Svarozic <petranikpeter@gmail.com>
2017-07-25 13:51:47 +0200

    -remove fittext jquery plugin -fit header to mobile devices

--- src/initExtJsPlugins.js
+++ src/initExtJsPlugins.js
@@ -1,25 +1,11 @@
-/**
- * Content of original ceevee-init.js
- */
-
 export default function () {
     window.jQuery(document).ready(function ($) {
 
-        /*----------------------------------------------------*/
-        /* FitText Settings
-         ------------------------------------------------------ */
-        setTimeout(function () {
-            $('.pp-hello.jquery-fittext').fitText(1, {minFontSize: '50px', maxFontSize: '90px'});
-        }, 100);
-
-
-        /*----------------------------------------------------*/
-        /* Smooth Scrolling
-         ------------------------------------------------------ */
+        // Smooth Scrolling
         $('.smoothscroll').on('click', function (e) {
             e.preventDefault();
 
-            var target = this.hash,
+            let target = this.hash,
                 $target = $(target);
 
             $('html, body').stop().animate({
