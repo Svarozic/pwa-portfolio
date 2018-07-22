74d9252826270572eea24c2c8f52da4df1d3e88e
Svarožič <petranikpeter@gmail.com>
2018-07-22 21:40:35 +0200

    add few new skills

--- src/components/PlaygroundSection/skills/index.js
+++ src/components/PlaygroundSection/skills/index.js
@@ -5,20 +5,25 @@ import pngAgular2 from './angular2.png';
 import pngAnt from './ant.png';
 import pngApache from './apache-webserver.png';
 import pngAtom from './atom.jpg';
+import pngAzure from './azure.jpg';
+import pngAzureCosmosDB from './azure-cosmosdb.png';
+import pngAzureFunctionApps from './azure-function-apps.jpg';
 import pngBackboneJS from './backbone.png';
 import pngBash from './bash.png';
 import pngBootstrap3 from './bootstrap.png';
 import pngBower from './bower.png';
 import pngCSharp from './c_sharp.jpg';
 import pngChromeExt from './chrome-extension.png';
+import pngConfluence from './confluence.jpg';
 import pngCSS3 from './css3.png';
 import pngDocker from './docker.png';
+import pngDockerCompose from './docker-compose.png';
 import pngEclipse from './eclipse.png';
 import pngEclisePlugins from './eclipse-plugins.png';
 import pngExpress from './express.png';
 import pngFedora from './fedora.jpg';
 import pngGimp from './gimp.png';
-import pngGit from './github.png';
+import pngGit from './git.png';
 import pngGnomeShell from './gnome-shell.jpg';
 import pngGradle from './gradle.png';
 import pngGrunt from './grunt.png';
@@ -26,6 +31,7 @@ import pngGulp from './gulp.png';
 import pngGWT from './gwt.jpg';
 import pngHibernate from './hibernate.png';
 import pngHtml from './html.png';
+import pngI18Next from './i18next.png';
 import pngIntelliJ from './intellij.jpg';
 import pngIvy from './ivy.png';
 import pngJade from './jade.png';
@@ -36,6 +42,8 @@ import pngJenkins from './jenkins.png';
 import pngJira from './jira.png';
 import pngJQuery from './jquery.png';
 import pngJUnit from './junit.jpg';
+import pngKeycloak from './keycloak.png';
+import pngKotlin from './kotlin.png';
 import pngLess from './less.png';
 import pngLinux from './linux.png';
 import pngMaterializeCss from './materializecss.png';
@@ -50,6 +58,8 @@ import pngPostgre from './postgresql.png';
 import pngReact from './reactjs.jpg';
 import pngRedux from './redux.png';
 import pngRequireJS from './requirejs.png';
+import pngRx from './rx.png';
+import pngSass from './sass.png';
 import pngScrum from './scrum.png';
 import pngSelenium from './selenium.jpg';
 import pngSemanticUI from './semanticui.jpg';
@@ -60,7 +70,10 @@ import pngSqLite from './sqlite.jpg';
 import pngSubversion from './subversion.png';
 import pngTypeScript from './typescript.png';
 import pngVaadin from './vaadin.png';
+import pngVertX from './vertx.png';
 import pngVisualStudio from './visual_studio.png';
+import pngVsTeamServices from './vs-team-services.png';
+import pngVSCode from './vscode.jpg';
 import pngWebpack from './webpack.jpg';
 import pngWebstorm from './webstorm.jpg';
 import pngWebSockets from './websockets.png';
@@ -68,6 +81,7 @@ import pngWP7 from './windows_phone.jpg';
 import pngXNA from './xna.png';
 import pngXpath from './xpath.png';
 import pngYeoman from './yeoman.png';
+import pngZeplin from './zeplin.png';
 
 export default [
     {title: 'Adobe Illustrator CS6', src: pngAdobeIllustrator},
@@ -77,15 +91,19 @@ export default [
     {title: 'Apache Ant', src: pngAnt},
     {title: 'Apache HTTP Server', src: pngApache},
     {title: 'Atom editor', src: pngAtom},
+    {title: 'Microsoft Azure', src: pngAzure},
+    {title: 'Microsoft Azure FunctionApp', src: pngAzureFunctionApps},
+    {title: 'Microsoft Azure CosmosDB', src: pngAzureCosmosDB},
     {title: 'BackboneJS', src: pngBackboneJS},
     {title: 'Bash', src: pngBash},
     {title: 'Boostrap 3', src: pngBootstrap3},
-
     {title: 'Bower', src: pngBower},
     {title: 'C#', src: pngCSharp},
     {title: 'Chrome Extensions', src: pngChromeExt},
+    {title: 'Confluence', src: pngConfluence},
     {title: 'CSS 3', src: pngCSS3},
     {title: 'Docker', src: pngDocker},
+    {title: 'Docker Compose', src: pngDockerCompose},
     {title: 'Eclipse', src: pngEclipse},
     {title: 'Eclipse Plugins', src: pngEclisePlugins},
     {title: 'ExpressJS', src: pngExpress},
@@ -93,29 +111,29 @@ export default [
     {title: 'Gimp', src: pngGimp},
     {title: 'Git', src: pngGit},
     {title: 'Gnome Shell', src: pngGnomeShell},
-
     {title: 'Gradle', src: pngGradle},
     {title: 'Grunt', src: pngGrunt},
     {title: 'Gulp', src: pngGulp},
     {title: 'Google Web Toolkit', src: pngGWT},
     {title: 'Hibernate', src: pngHibernate},
     {title: 'HTML 5', src: pngHtml},
+    {title: 'i18next', src: pngI18Next},
     {title: 'IntelliJ Idea', src: pngIntelliJ},
     {title: 'Apache Ivy', src: pngIvy},
     {title: 'Jade', src: pngJade},
     {title: 'Java', src: pngJava},
-
     {title: 'JAVA EE', src: pngJavaEE},
     {title: 'JavaScript', src: pngJavaScript},
     {title: 'Jenkins CI', src: pngJenkins},
     {title: 'Jira', src: pngJira},
     {title: 'JQuery', src: pngJQuery},
     {title: 'JUnit', src: pngJUnit},
+    {title: 'Keycloak', src: pngKeycloak},
+    {title: 'Kotlin', src: pngKotlin},
     {title: 'Less Css', src: pngLess},
     {title: 'Linux', src: pngLinux},
     {title: 'MaterializeCSS', src: pngMaterializeCss},
     {title: 'Maven', src: pngMaven},
-
     {title: 'MongoDB', src: pngMongoDB},
     {title: 'MySQL', src: pngMsql},
     {title: 'NodeJS', src: pngNodeJS},
@@ -126,7 +144,8 @@ export default [
     {title: 'ReactJS', src: pngReact},
     {title: 'Redux', src: pngRedux},
     {title: 'RequireJS', src: pngRequireJS},
-
+    {title: 'RX', src: pngRx},
+    {title: 'Sass', src: pngSass},
     {title: 'Scrum Agile', src: pngScrum},
     {title: 'Selenium test framework', src: pngSelenium},
     {title: 'Semantic UI', src: pngSemanticUI},
@@ -137,13 +156,16 @@ export default [
     {title: 'Subversion', src: pngSubversion},
     {title: 'Typescript', src: pngTypeScript},
     {title: 'Vaadin 6', src: pngVaadin},
-
+    {title: 'Vert.X', src: pngVertX},
     {title: 'Visual Studio 2010', src: pngVisualStudio},
+    {title: 'VS Team Services', src: pngVsTeamServices},
+    {title: 'VS Code', src: pngVSCode},
     {title: 'Webpack Module Bundler', src: pngWebpack},
     {title: 'WebStorm', src: pngWebstorm},
     {title: 'Websockets', src: pngWebSockets},
     {title: 'Windows Phone 7.5', src: pngWP7},
     {title: 'XNA', src: pngXNA},
     {title: 'XPath', src: pngXpath},
-    {title: 'Yeoman', src: pngYeoman}
+    {title: 'Yeoman', src: pngYeoman},
+    {title: 'Zeplin', src: pngZeplin}
 ];
